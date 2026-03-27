05d0346d0db966eece08ebd9c1e860918b7eaa4d
ohmzi <6551272+ohmzi@users.noreply.github.com>
2026-03-27 06:40:52 -0400

    fix(android): restore standalone compose build

--- android-compose/app/build.gradle.kts
+++ android-compose/app/build.gradle.kts
@@ -100,8 +100,8 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.8.5")
     implementation("com.google.android.material:material:1.12.0")
 
-    implementation("com.google.dagger:hilt-android:2.52")
-    ksp("com.google.dagger:hilt-compiler:2.52")
+    implementation("com.google.dagger:hilt-android:2.57.2")
+    ksp("com.google.dagger:hilt-compiler:2.57.2")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
     implementation("androidx.hilt:hilt-work:1.2.0")
     ksp("androidx.hilt:hilt-compiler:1.2.0")
