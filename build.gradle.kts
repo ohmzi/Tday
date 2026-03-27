35be38af5f44a6a4b93a70954d6e12fa6ce24efc
Omar <omariqbal97@gmail.com>
2026-03-27 06:40:52 -0400

    fix(android): restore standalone compose build

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     id("org.jetbrains.kotlin.plugin.serialization") version "2.1.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
     id("com.google.devtools.ksp") version "2.1.0-1.0.29" apply false
-    id("com.google.dagger.hilt.android") version "2.52" apply false
+    id("com.google.dagger.hilt.android") version "2.57.2" apply false
     id("io.ktor.plugin") version "3.0.3" apply false
 }
 
