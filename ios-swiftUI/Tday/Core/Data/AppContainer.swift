f2231909ea21392b26919dcf0c83e027fda4084d
ohmzi <6551272+ohmzi@users.noreply.github.com>
2026-05-22 15:51:09 -0400

    Fix iOS reinstall server URL cleanup

--- ios-swiftUI/Tday/Core/Data/AppContainer.swift
+++ ios-swiftUI/Tday/Core/Data/AppContainer.swift
@@ -34,6 +34,7 @@ final class AppContainer {
 
     private init() {
         secureStore = SecureStore()
+        secureStore.clearInstallScopedKeychainValuesIfAppReinstalled()
         themeStore = ThemeStore()
         reminderPreferenceStore = ReminderPreferenceStore()
         serverURLState = ServerURLState(currentURL: secureStore.loadPersistedServerURL())
